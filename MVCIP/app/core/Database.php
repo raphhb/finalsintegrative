c0599e40f36013bc5d7ea12a9859b5a59f83c25a
rafael.d.mrtinez@gmail.com <raffimartin069@gmail.com>
2024-05-18 13:13:34 +0800

    Added Changes 5/18/2024:
    Views:
    - movie.view.php
    - Updated bootstrap icons cdn.
    - Switch to local bootstrap file.
    
    Controller:
    - MovieController.php (Please check 2 Movie class exist, one for admin and one for Movie selection).
    - Added necessary method for data manipulation and rendering guides.
    - Added .htacess in public folder.
    
    CSS:
    - Created a folder movie under public/assets/css/movie to separate movie selection page css.

--- MVCIP/app/core/Database.php
+++ MVCIP/app/core/Database.php
@@ -1,53 +1,49 @@
-<?php 
+<?php
 
-Trait Database
+trait Database
 {
 
-	private function connect()
-	{
-		$string = "mysql:hostname=".DBHOST.";dbname=".DBNAME;
-		$con = new PDO($string,DBUSER,DBPASS);
-		return $con;
-	}
+    private function connect()
+    {
+        $string = "mysql:hostname=" . DBHOST . ";dbname=" . DBNAME;
+        $con = new PDO($string, DBUSER, DBPASS);
+        return $con;
+    }
 
-	public function query($query, $data = [])
-	{
+    public function query($query, $data = [])
+    {
 
-		$con = $this->connect();
-		$stm = $con->prepare($query);
+        $con = $this->connect();
+        $stm = $con->prepare($query);
 
-		$check = $stm->execute($data);
-		if($check)
-		{
-			$result = $stm->fetchAll(PDO::FETCH_OBJ);
-			if(is_array($result) && count($result))
-			{
-				return $result;
-			}
-		}
+        $check = $stm->execute($data);
+        if ($check) {
+            $result = $stm->fetchAll(PDO::FETCH_OBJ);
+            if (is_array($result) && count($result)) {
+                return $result;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public function get_row($query, $data = [])
-	{
+    public function get_row($query, $data = [])
+    {
 
-		$con = $this->connect();
-		$stm = $con->prepare($query);
+        $con = $this->connect();
+        $stm = $con->prepare($query);
 
-		$check = $stm->execute($data);
-		if($check)
-		{
-			$result = $stm->fetchAll(PDO::FETCH_OBJ);
-			if(is_array($result) && count($result))
-			{
-				return $result[0];
-			}
-		}
+        $check = $stm->execute($data);
+        if ($check) {
+            $result = $stm->fetchAll(PDO::FETCH_OBJ);
+            if (is_array($result) && count($result)) {
+                return $result[0];
+            }
+        }
 
-		return false;
-	}
-	
+        return false;
+    }
+
 }
 
 
